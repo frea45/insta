ba6bf09c5b751049df71c9ff1024c1858326c1ac
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-08 14:17:25 +0330

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-API_ID = 12345678  # api_id تلگرام
-API_HASH = "your_api_hash"  # api_hash تلگرام
-BOT_TOKEN = "your_bot_token"  # توکن ربات
-
-LOG_CHANNEL = -1001792962793  # آیدی عددی کانال لاگ
-PROXY = None  # اگر پروکسی می‌خوای اضافه کنی به شکل {"https": "http://ip:port"}
