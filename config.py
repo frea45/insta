eb66fefb01c126b95d158ee616c51c3647e26c01
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-08 12:23:26 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID = int(os.environ.get("API_ID", 12345))
+API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
+BOT_TOKEN = os.environ.get("BOT_TOKEN ", "")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001792962793")
+ADMIN_ID = os.environ.get("ADMIN_ID", "763990585")
