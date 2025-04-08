20bc78df68b34ce5c912079deb5f95ce378500b1
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-08 14:03:15 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", 3335796))
     API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5088657122:AAEPiacGAxa-9fQ19keruQua74tf2-Ljktk")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7136875110:AAFzyr2i2FbRrmst1sklkJPN7Yz2rXJvSew")
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001792962793")
     ADMIN_ID = os.environ.get("ADMIN_ID", "763990585")
