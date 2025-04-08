50e9e61309814cf0f8a33fece566fba874b15034
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-08 12:58:58 +0330

    Update instagram_bot.py

--- instagram_bot.py
+++ instagram_bot.py
@@ -9,8 +9,12 @@ import requests
 
 
 
-app = Client("instagram_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
+app = Client(
+    "instagram_bot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
+)
 loader = instaloader.Instaloader(dirname_pattern=tempfile.gettempdir(), download_videos=False,
                                  download_video_thumbnails=False, download_comments=False,
                                  save_metadata=False, compress_json=False)
